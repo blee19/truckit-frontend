9f3c84b824d9c51557eb16972ffd71b7da4ad803
Ethan Zou <eazy975@gmail.com>
2017-01-20 14:13:59 -0500

    history modal looks nice and "tight"

--- app/js/script.js
+++ app/js/script.js
@@ -81,12 +81,11 @@ function loginInit(info) {
 	logout.innerHTML = 'Logout';
 	navbar.appendChild(logout);
 
-	var history = document.createElement('a');
-	history.setAttribute('class', 'quiet-link navbar-item');
+	var history = document.createElement('button');
+	history.setAttribute('class', 'button btn btn-lg pull-right navbar-btn');
 	history.innerHTML = 'History';
 	history.setAttribute('data-target','#history-modal')
 	history.setAttribute('data-toggle','modal')
-	history.setAttribute('onclick', '')
 	navbar.appendChild(history);
 }
 
